a20fb3b68c243fb5d179370936091c5f68f0c60f
Sunil Yadav <sy4520360@gmail.com>
2021-11-12 18:13:16 +0530

    Performed cancel payment functionality

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -46,6 +46,6 @@ class PaymentController extends Controller
 
     public function cancelled()
     {
-        
+        return redirect()->route('home')->withErrors('You cancelled the payment');
     }
 }
